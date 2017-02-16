2311f6a5dec72c7ce3e1aea5bb38dc1241c25bbd
Brandon Brigance <brandon.brigance@gmail.com>
2017-02-15 22:49:19 -0600

    migration: products price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215231556) do
+ActiveRecord::Schema.define(version: 20170216043646) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170215231556) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.integer  "price"
   end
 
 end
